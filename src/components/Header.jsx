d0772d122be9fc0f7d93330b3f5fe4fb57d62ee2
viettu2k <doanviettu2k@gmail.com>
2021-08-20 08:44:55 +0700

    setup header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div className="header-main">
+            <h1 className="name-header">Hi, I am Doan Viet Tu </h1> ;
+        </div>
+    );
+}
